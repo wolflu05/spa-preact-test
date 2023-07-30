03f6c2046f7b094cd9eb6dfe556846539994aa8c
wolflu05 <76838159+wolflu05@users.noreply.github.com>
2023-07-30 22:47:10 +0200

    Remove unused proxy config

--- vite-spa/vite.config.ts
+++ vite-spa/vite.config.ts
@@ -4,23 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/react-app/": {
-        target: "http://localhost:4444",
-        changeOrigin: true,
-        secure: true
-      },
-      "/my-ts-app/": {
-        target: "http://localhost:4444",
-        changeOrigin: true,
-        secure: true
-      },
-      "/preact-app/": {
-        target: "http://localhost:4444",
-        changeOrigin: true,
-        secure: true
-      }
-    }
-  }
 })
